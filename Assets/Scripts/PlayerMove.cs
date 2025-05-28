ea0ba9f592f4891161cc36ee61cc92c0001951be
ActiveP <patrykos65a@gmail.com>
2025-05-28 21:35:02 +0200

    BasicMovement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float speed = 5f;
+    float horizontalMovement = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.linearVelocity = new Vector2(horizontalMovement * speed, rb.linearVelocityY);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
     }
 }
