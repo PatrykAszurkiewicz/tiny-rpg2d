98668e23e71cb8a88a2f681d412bb7fd28b4554a
ActiveP <patrykos65a@gmail.com>
2025-05-29 23:24:37 +0200

    Update PlayerMove.cs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,8 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rb;
     public float speed = 5f;
     float horizontalMovement = 0f;
+    float jumpForce = 10f;
+    bool isGrounded = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,5 +24,18 @@ public class PlayerMove : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+            isGrounded = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Zak³adamy, ¿e wszystko co ma Collider to ziemia – mo¿na to rozwin¹æ
+        isGrounded = true;
     }
 }
