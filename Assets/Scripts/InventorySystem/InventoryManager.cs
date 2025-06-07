d87d61cf1b11019bf6f827c513c9eed94605d220
ActiveP <patrykos65a@gmail.com>
2025-06-07 16:07:36 +0200

    Inventory fixes

--- Assets/Scripts/InventorySystem/InventoryManager.cs
+++ Assets/Scripts/InventorySystem/InventoryManager.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
@@ -5,17 +7,67 @@ public class InventoryManager : MonoBehaviour
     public GameObject slotPrefab;
     public Transform slotsParent;
     public int NumberOfSlots = 10;
+
+    public ItemData testItem1; //for testing
+    public ItemData testItem2;
+    public ItemData testItem3;
+
+    private List<InventorySlot> inventorySlots = new List<InventorySlot>();
+    public List<InventorySlotUI> itemsInfo = new List<InventorySlotUI>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GenerateSlots();
+
+        AddItem(testItem1, 3);
+        AddItem(testItem2, 15);
+        AddItem(testItem3, 37);
     }
 
     void GenerateSlots()
     {
         for(int i = 0; i < NumberOfSlots; i++)
         {
+            InventorySlot newSlot = new InventorySlot();
+            inventorySlots.Add(newSlot);
+
             GameObject slot = Instantiate(slotPrefab, slotsParent);
+            InventorySlotUI slotUI = slot.GetComponent<InventorySlotUI>();
+            slotUI.SetSlot(newSlot);
+            itemsInfo.Add(slotUI);
+        }
+    }
+
+    public void AddItem(ItemData itemToAdd, int quantity)
+    {
+        foreach (var slot in inventorySlots)
+        {
+            if (!slot.IsEmpty && slot.item == itemToAdd && itemToAdd.isStackable)
+            {
+                slot.quantity += quantity;
+                UpdateUI();
+                return;
+            }
+        }
+
+        foreach (var slot in inventorySlots)
+        {
+            if (slot.IsEmpty)
+            {
+                slot.item = itemToAdd;
+                slot.quantity = quantity;
+                UpdateUI();
+                return;
+            }
+        }
+    }
+
+    private void UpdateUI()
+    {
+        for (int i = 0; i < itemsInfo.Count; i++)
+        {
+            itemsInfo[i].SetSlot(inventorySlots[i]);
         }
     }
 }
--- Assets/Scripts/InventorySystem/InventorySlotUI.cs
+++ Assets/Scripts/InventorySystem/InventorySlotUI.cs
@@ -6,6 +6,7 @@ public class InventorySlotUI : MonoBehaviour
 {
     private Image iconImage;
     private TextMeshProUGUI quantityText;
+    private InventorySlot slotData;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +15,32 @@ public class InventorySlotUI : MonoBehaviour
 
         iconImage.enabled = false;
         quantityText.text = "";
+    }
+    public void SetSlot(InventorySlot slot)
+    {
+        slotData = slot;
+        RefreshUI();
+    }
+    public void RefreshUI()
+    {
+        if (slotData == null || slotData.IsEmpty)
+        {
+            iconImage.enabled = false;
+            quantityText.text = "";
+        }
+        else
+        {
+            iconImage.enabled = true;
+            iconImage.sprite = slotData.item.icon;
+
+            if (slotData.item.isStackable && slotData.quantity > 1)
+            {
+                quantityText.text = slotData.quantity.ToString();
+            }
+            else
+            {
+                quantityText.text = "";
+            }
+        }
     }
 }
