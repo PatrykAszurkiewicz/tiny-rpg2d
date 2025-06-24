74f4595e38b65c502379e2e2f4dda5c897d75868
ActiveP <patrykos65a@gmail.com>
2025-06-24 19:07:53 +0200

    Update ResourceBarUI.cs

--- Assets/Scripts/Bars/ResourceBarUI.cs
+++ Assets/Scripts/Bars/ResourceBarUI.cs
@@ -4,17 +4,46 @@ using UnityEngine.UI;
 public class ResourceBarUI : MonoBehaviour
 {
     [SerializeField] private Image fillImage;
+    [SerializeField] private Image shadowImage; 
 
     private float targetFill = 1f;
     private float lerpSpeed = 10f;
 
+    private float shadowLerpSpeed = 2f;
+    private float shadowDelay = 0.5f;
+    private float shadowTimer = 0f;
+    private bool hasShadow => shadowImage != null;
+
     public void SetValue(float current, float max)
     {
-        targetFill = Mathf.Clamp01(current / max);
+        float newTarget = Mathf.Clamp01(current / max);
+
+        if (!Mathf.Approximately(newTarget, targetFill))
+        {
+            targetFill = newTarget;
+
+            if (hasShadow)
+            {
+                shadowTimer = shadowDelay;
+            }
+        }
     }
 
     private void Update()
     {
+        
         fillImage.fillAmount = Mathf.Lerp(fillImage.fillAmount, targetFill, Time.deltaTime * lerpSpeed);
+
+        if (hasShadow)
+        {
+            if (shadowTimer > 0)
+            {
+                shadowTimer -= Time.deltaTime;
+            }
+            else
+            {
+                shadowImage.fillAmount = Mathf.Lerp(shadowImage.fillAmount, targetFill, Time.deltaTime * shadowLerpSpeed);
+            }
+        }
     }
 }
